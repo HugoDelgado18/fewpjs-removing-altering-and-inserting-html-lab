02379cb6d7b93172609863f1fcd386945af25ce5
hugodelgado <hugo.delgado1@outlook.com>
2020-11-17 16:01:34 -0600

    commit tree f006471e6bd23af3fcf5e721f68edaba3eba1ab3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let mainTag = document.body.querySelector('#main')
+mainTag.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "YOUR-NAME is the champion"
+let body = document.body
+
+body.appendChild(newHeader)
